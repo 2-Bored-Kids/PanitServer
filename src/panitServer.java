81d2a44bb0b1d760fdd3fc86ea224d3194f9e519
Adivius <adivius@chaos-siegen.de>
2022-10-08 20:53:07 +0200

    Added local server support

--- src/panitServer.java
+++ src/panitServer.java
@@ -1,5 +1,7 @@
 import sum.netz.*;
 
+import java.util.Objects;
+
 public class panitServer extends Server{
 
     public panitServer(int port){
@@ -7,32 +9,29 @@ public class panitServer extends Server{
     }
 
     public void bearbeiteVerbindungsaufbau(String ip, int port){
-        sendeAnAlle(ip + " connected");
-        sendeAnEinen(ip, packetIds.PACKET_CONNECT+ "");
+        sendeAnEinen(ip, port,packetIds.PACKET_CONNECT+ "");
     }
 
-    public void bearbeiteNachricht(String ip, String message){
+    public void bearbeiteNachricht(String ip, int port, String message){
 
         String[] packet = message.split(packetIds.SEPARATOR + "");
 
-        if (packet[0] == packetIds.PACKET_DISCONNECT + ""){
-            schliesseVerbindung(ip);
+        if (Objects.equals(packet[0], packetIds.PACKET_DISCONNECT + "")){
+            schliesseVerbindung(ip, port);
         }else {
             sendeAnAlle(message);
         }
     }
 
     public void bearbeiteVerbindungsverlust(String ip, int port){
-        sendeAnAlle(ip + " disconnected");
-        sendeAnEinen(ip, packetIds.PACKET_DISCONNECT + "");
+        sendeAnEinen(ip, port, packetIds.PACKET_DISCONNECT + "");
     }
 
     public void bearbeiteVerbindungsende(String ip, int port){
-        sendeAnAlle(ip + " disconnected");
-        sendeAnEinen(ip, packetIds.PACKET_DISCONNECT + "");
+        sendeAnEinen(ip, port, packetIds.PACKET_DISCONNECT + "");
     }
 
     static public void main(String[] args){
-        panitServer main = new panitServer(Integer.parseInt(args[0]));
+        new panitServer(Integer.parseInt(args[0]));
     }
 }
