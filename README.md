36a2a5e8a54caecf528aa1125319c2bc1b224c59
Adivius <adivius@chaos-siegen.de>
2022-10-08 18:24:48 +0200

    Changed packetIds

--- README.md
+++
@@ -1,2 +0,0 @@
-# PanitServer
-Server for Panit
---
+++ src/packetIds.java
@@ -0,0 +1,7 @@
+public class packetIds {
+
+    public final static char SEPARATOR = ':';
+
+    public final static int PACKET_CONNECT = 1, PACKET_DISCONNECT = 2, PACKET_RUNTER = 3, PACKET_HOCH = 4, PACKET_MOVE = 5, PACKET_WIDTH = 6;
+
+}
