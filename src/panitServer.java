057f435c3e347ad9f21ebd3e603c77bcaac6d354
Adivius <adivius@chaos-siegen.de>
2022-10-10 01:12:48 +0200

    Changed messages

--- src/panitServer.java
+++ src/panitServer.java
@@ -3,11 +3,13 @@ import sum.netz.*;
 public class panitServer extends Server {
 
     public panitServer(int port) {
-        super(port, true);
+        super(port, false);
+        System.out.println("Server wurde unter dem Port " + port + " gestartet!\n");
     }
 
     public void bearbeiteVerbindungsaufbau(String ip, int port) {
         sendeAnEinen(ip, port, Integer.toString(packetIds.PACKET_CONNECT));
+        System.out.println("Neue Verbindung: " + ip + ":" + Integer.toString(port) + "\nVerbindungen: " + this.zahlDerVerbindungen() + "\n");
     }
 
     public void bearbeiteNachricht(String ip, int port, String message) {
@@ -23,10 +25,12 @@ public class panitServer extends Server {
 
     public void bearbeiteVerbindungsverlust(String ip, int port) {
         sendeAnEinen(ip, port, Integer.toString(packetIds.PACKET_DISCONNECT));
+        System.out.println("Verbindung getrennt: " + ip + ":" + Integer.toString(port) + "\nVerbindungen: " + this.zahlDerVerbindungen());
     }
 
     public void bearbeiteVerbindungsende(String ip, int port) {
         sendeAnEinen(ip, port, Integer.toString(packetIds.PACKET_DISCONNECT));
+        System.out.println("Verbindung getrennt: " + ip + ":" + Integer.toString(port) + "\nVerbindungen: " + this.zahlDerVerbindungen());
     }
 
     static public void main(String[] args) {
